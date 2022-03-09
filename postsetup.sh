5f4ce3cca47c3e0143f5544468fe12917b600451
Ebby <ebby@Ebbys-MBP.cable.rcn.com>
2022-03-09 01:20:07 -0500

    systemD

--- postsetup.sh
+++ postsetup.sh
@@ -3,8 +3,11 @@ cd webservice
 sudo mvn install -DskipTests=true
 sudo chmod 777 ./target/assignment-1.0-SNAPSHOT.jar
 cd ..
-echo '#!/bin/bash 
-java -DMYSQL_PASSWORD=${MYSQL_PASSWORD} -DS3_BUCKETNAME=${S3_BUCKETNAME} -DMYSQL_HOST=${MYSQL_HOST} -DMYSQL_USER=${MYSQL_USER} -DMYSQL_DATABASE=${MYSQL_DATABASE} -jar home/ec2-user/webservice/target/assignment-1.0-SNAPSHOT.jar' > runjar.sh
+echo '#!/bin/bash
+
+source /etc/profile.d/custom.sh
+
+sudo java -DMYSQL_PASSWORD=${MYSQL_PASSWORD} -DS3_BUCKETNAME=${S3_BUCKETNAME} -DMYSQL_HOST=${MYSQL_HOST} -DMYSQL_USER=${MYSQL_USER} -DMYSQL_DATABASE=${MYSQL_DATABASE} -jar home/ec2-user/webservice/target/assignment-1.0-SNAPSHOT.jar' > runjar.sh
 
 sudo chmod 777 runjar.sh
 
