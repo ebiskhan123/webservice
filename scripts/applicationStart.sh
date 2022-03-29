3c782e36b79c74b235ad1934c3de6e66ff43f7df
Ebby <ebby@Ebbys-MacBook-Pro.local>
2022-03-29 19:11:49 -0400

    changed to one script

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,4 +1,28 @@
+#sudo systemctl stop mywebservice.service
+#sudo systemd-resolve --flush-caches
+#sudo systemctl daemon-reload
+#sudo systemctl start mywebservice.service
+
+
 sudo systemctl stop mywebservice.service
-sudo systemd-resolve --flush-caches
+sudo systemctl disable mywebservice.service
 sudo systemctl daemon-reload
-sudo systemctl start mywebservice.service
+cd /home/ec2-user
+cd webservice
+sudo mvn install -DskipTests=true
+sudo chmod 777 ./target/assignment-1.0-SNAPSHOT.jar
+cd ..
+pwd
+
+echo '#!/bin/bash
+
+sleep 10
+source /etc/profile.d/custom.sh
+
+sudo java -DMYSQL_PASSWORD=${MYSQL_PASSWORD} -DS3_BUCKETNAME=${S3_BUCKETNAME} -DMYSQL_HOST=${MYSQL_HOST} -DMYSQL_USER=${MYSQL_USER} -DMYSQL_DATABASE=${MYSQL_DATABASE} -jar home/ec2-user/webservice/target/assignment-1.0-SNAPSHOT.jar' > runjar.sh
+
+sudo chmod 777 runjar.sh
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable mywebservice.service
