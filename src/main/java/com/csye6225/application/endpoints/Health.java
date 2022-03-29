db095bc2f8a21496347559be4c154c9655da14ef
Ebby <ebby@Ebbys-MacBook-Pro.local>
2022-03-29 15:25:42 -0400

    heal

--- src/main/java/com/csye6225/application/endpoints/Health.java
+++ src/main/java/com/csye6225/application/endpoints/Health.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Service
 @RestController
-@RequestMapping("/healthz:")
+@RequestMapping("/heal")
 public class Health {
 
     // assignment 1 api
