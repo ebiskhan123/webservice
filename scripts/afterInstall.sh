7ffdf76fab3407c107adb7824b9a94b2e5514baa
Ebby <ebby@Ebbys-MacBook-Pro.local>
2022-03-29 18:45:52 -0400

    changing the shell script

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -8,4 +8,13 @@ cd ..
 pwd
 sudo systemctl daemon-reload
 
+echo '#!/bin/bash
+
+sleep 10
+source /etc/profile.d/custom.sh
+
+sudo java -DMYSQL_PASSWORD=${MYSQL_PASSWORD} -DS3_BUCKETNAME=${S3_BUCKETNAME} -DMYSQL_HOST=${MYSQL_HOST} -DMYSQL_USER=${MYSQL_USER} -DMYSQL_DATABASE=${MYSQL_DATABASE} -jar home/ec2-user/webservice/target/assignment-1.0-SNAPSHOT.jar' > runjar.sh
+
+sudo chmod 777 runjar.sh
+
 
