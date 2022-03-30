ef3980d1e8e5c1390c3f21902d0bc88c35232d5a
Ebby <ebby@Ebbys-MacBook-Pro.local>
2022-03-29 22:07:34 -0400

    .

--- src/main/java/com/csye6225/application/endpoints/Health.java
+++ src/main/java/com/csye6225/application/endpoints/Health.java
@@ -17,7 +17,7 @@ public class Health {
         return ResponseEntity.ok().body(null);
     }
 
-    @GetMapping(value = "/demoz")
+    @GetMapping(value = "/jason")
     public ResponseEntity<?> getHealth(){
         return ResponseEntity.ok().body(null);
     }
